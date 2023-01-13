6160b9b8036e646912d70b12eda0b780d1e96495
Liezelle20 <111780566+Liezelle20@users.noreply.github.com>
2023-01-13 21:47:40 +0200

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class day4 {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        int sum = 0;
+        ;
+        while (i <= 5) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number : ");
+            int num = sc.nextInt();
+            sum = sum + num;
+            i++;
+
+        }
+        System.out.println("the sum of all the 5 integers is " + sum);
+    }
+}
